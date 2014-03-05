b6787c89f1d4f88795643db29833326ee919b6e1
roxanarugina <roxana.rugina@gmail.com>
2014-03-05 13:53:18 +0100

    add static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def places
+  end
+
+  def events
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get places" do
+    get :places
+    assert_response :success
+  end
+
+  test "should get events" do
+    get :events
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+end
