a54b8a91042565793448f78f302406d5b32c26f4
roxanarugina <roxana.rugina@gmail.com>
2014-03-05 12:51:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bicyclink::Application.config.secret_key_base = '25115d9b25530bd210284a275e3199bb9f3faac6f09db045df688c031bc64eb86c3f1a7bdd4fd251b922f0cd9139842bb3a7e3a6e8cb78cb6e68ba55e723eca2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bicyclink::Application.config.session_store :cookie_store, key: '_bicyclink_session'
