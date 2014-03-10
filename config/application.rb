373478769b970302e47c42f42b11297cac323ff2
roxanarugina <roxana.rugina@gmail.com>
2014-03-10 20:05:38 +0100

    config assets

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
 module Bicyclink
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,5 +24,4 @@ module Bicyclink
     # config.i18n.default_locale = :de
   end
 end
-
-config.assets.initialize_on_precompile = false
+e
