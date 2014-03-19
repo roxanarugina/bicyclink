722c741dd964f790a791a36315238114e354d164
Mori <keitamori@gmail.com>
2014-03-19 14:17:28 +0100

    Creation du code leaflet de base pour l'application - javascript

--- app/assets/javascripts/carte.js
+++ app/assets/javascripts/carte.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://www.mosleymos.cartodb.com//997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
+
+
