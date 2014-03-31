86db90ee8354e9729612498f426c539e95d3e03b
camilledelbos <camille.delbos@gmail.com>
2014-03-31 15:11:33 +0200

    nettoyage et ajout de Leaflet. Géolocalisation ok

--- app/assets/javascripts/carte.js
+++ app/assets/javascripts/carte.js
@@ -4,9 +4,12 @@
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('http://www.mosleymos.cartodb.com//997/256/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.mapbox.com/v3/foursquare.meku766r/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
 
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
+
+var lc = L.control.locate({position: 'topleft'}).addTo(map);
--- app/assets/javascripts/carte2db.js
+++
@@ -1,5 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-
-cartodb.createVis('map2','http://mosleymos.cartodb.com/api/v2/viz/c0b9042e-af64-11e3-978b-0e73339ffa50/viz.json');
